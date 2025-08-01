f63b986d2546a820d101379449478afe65f80b70
Розова София <rozowasof@yandex.ru>
2025-08-01 19:27:03 +0300

    the basic functionality of the Category and Product classes has been implemented

---
+++ tests/test_product.py
@@ -0,0 +1,5 @@
+def test_product(test_product):
+    assert test_product.name == 'Apples'
+    assert test_product.description == 'Juicy golden apples'
+    assert test_product.price == 67.9
+    assert test_product.quantity == 90
