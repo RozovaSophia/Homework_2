37d0c2e20d55d56e7498e664791ad4e68f886de7
Розова София <rozowasof@yandex.ru>
2025-08-02 10:11:35 +0300

    implemented functionality that reads a json file

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+
 class Product:
     name: str
     description: str
@@ -16,18 +20,38 @@ class Category:
     description: str
     products: list
 
-    count_category = 0
-    count_product = 0
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products if products else []
-        Category.count_category += 1
-        Category.count_product = len(products) if products else 0
+        Category.category_count += 1
+        Category.product_count = len(products) if products else 0
 
 
+def read_json(path: str) -> dict:
+    full_path = os.path.abspath(path)
+    with open(full_path, "r", encoding="UTF-8") as f:
+        data = json.load(f)
+    return data
 
 
+def create_objects_from_json(data):
+    objects = []
+    for item in data:
+        products = []
+
+        for product in item["products"]:
+            products.append(Product(**product))
+
+        item["products"] = products
+        objects.append(Category(**item))
+
+    return objects
 
 
+if __name__ == "__main__":
+    result = create_objects_from_json(read_json("../data/products.json"))
+    print(result)
