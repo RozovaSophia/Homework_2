e9de83421bd469d374d2d3c28db3301d4c8a5da8
Розова София <rozowasof@yandex.ru>
2025-08-07 14:11:02 +0300

    the basic functionality is implemented

--- src/utils.py
+++ src/utils.py
@@ -11,9 +11,32 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+    @property
+    def products(self):
+        return f'{self.name}, {self.__price} руб.Остаток: {self.quantity} шт.'
+
+
+    @classmethod
+    def new_product(cls, product_data):
+
+        name = product_data["name"]
+        description = product_data["description"]
+        price = product_data["price"]
+        quantity = product_data["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if int(price) <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
 
 class Category:
     name: str
@@ -23,12 +46,22 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
-        Category.product_count = len(products) if products else 0
+        Category.product_count = len(self.__products)
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+
+
+    @property
+    def products(self):
+        return self.__products
 
 
 def read_json(path: str) -> dict:
@@ -53,5 +86,34 @@ def create_objects_from_json(data):
 
 
 if __name__ == "__main__":
-    result = create_objects_from_json(read_json("../data/products.json"))
-    print(result)
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3]
+    )
+
+    print(category1.products)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    print(category1.products)
+    print(category1.product_count)
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
